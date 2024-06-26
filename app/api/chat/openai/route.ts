6670eb256140ba7bf4104fab6a7f56fc3358e5ff
Vedagya Bhalotia <vedagya@samvid.ai>
2024-06-26 14:51:35 +0530

    only_input_cost

--- app/api/chat/openai/route.ts
+++ app/api/chat/openai/route.ts
@@ -1,9 +1,12 @@
 import { checkApiKey, getServerProfile } from "@/lib/server/server-chat-helpers"
-import { ChatSettings } from "@/types"
+import { ChatSettings, LLMID } from "@/types"
 import { OpenAIStream, StreamingTextResponse } from "ai"
 import { ServerRuntime } from "next"
 import OpenAI from "openai"
 import { ChatCompletionCreateParamsBase } from "openai/resources/chat/completions.mjs"
+import { calculateCost } from "@/lib/models/costfnction"
+import tokenizer from "gpt-tokenizer"
+import { OPENAI_LLM_LIST } from "@/lib/models/llm/openai-llm-list" // Assuming you saved the models in this file
 
 export const runtime: ServerRuntime = "edge"
 
@@ -24,21 +27,47 @@ export async function POST(request: Request) {
       organization: profile.openai_organization_id
     })
 
+    // Tokenize the last input message
+    const lastMessage = messages[messages.length - 1]
+    const inputTokens = tokenizer.encode(lastMessage.content).length
+    const maxTokens =
+      chatSettings.model === "gpt-4-vision-preview" ||
+      chatSettings.model === "gpt-4o"
+        ? 4096
+        : null
+
     const response = await openai.chat.completions.create({
       model: chatSettings.model as ChatCompletionCreateParamsBase["model"],
       messages: messages as ChatCompletionCreateParamsBase["messages"],
       temperature: chatSettings.temperature,
-      max_tokens:
-        chatSettings.model === "gpt-4-vision-preview" ||
-        chatSettings.model === "gpt-4o"
-          ? 4096
-          : null, // TODO: Fix
+      max_tokens: maxTokens,
       stream: true
     })
 
-    const stream = OpenAIStream(response)
+    // Process the response stream to count the output tokens
+    let outputTokens = 0
+    const stream = OpenAIStream(response, {
+      async onToken(token) {
+        outputTokens += tokenizer.encode(token).length
+        console.log(`output tokens in here: ${outputTokens}`)
+      }
+    })
 
-    return new StreamingTextResponse(stream)
+    const cost = calculateCost(
+      chatSettings.model as LLMID,
+      inputTokens,
+      outputTokens,
+      OPENAI_LLM_LIST
+    )
+    console.log(`output tokens: ${outputTokens}`)
+    console.log(`input tokens: ${inputTokens}`)
+    console.log(`Cost for the request: ${cost}`)
+
+    return new StreamingTextResponse(stream, {
+      headers: {
+        "X-Cost": cost.toString()
+      }
+    })
   } catch (error: any) {
     let errorMessage = error.message || "An unexpected error occurred"
     const errorCode = error.status || 500
